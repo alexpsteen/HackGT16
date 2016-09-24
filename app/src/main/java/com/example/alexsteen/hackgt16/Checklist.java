933673f03d3aae90f450d049d913b760d132a6cc
alexpsteen <alexpsteen@gmail.com>
2016-09-24 15:27:02 -0400

    change stuff

--- app/src/main/java/com/example/alexsteen/hackgt16/Checklist.java
+++ app/src/main/java/com/example/alexsteen/hackgt16/Checklist.java
@@ -2,16 +2,29 @@ package com.example.alexsteen.hackgt16;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.LinkedList;
 
 /**
  * Created by Alex Steen on 9/24/2016.
  */
 public class Checklist extends AppCompatActivity {
-
+    private LinkedList<Elist> checklist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist);
+        checklist = new LinkedList<>();
     }
+
+    public void addEntry(View view) {
+        EditText name = (EditText) findViewById(R.id.elistTB);
+
+        Elist e = new Elist(name.getText().toString());
+        checklist.add(e);
+    }
+
 }
--- app/src/main/java/com/example/alexsteen/hackgt16/WelcomeScreen.java
+++ app/src/main/java/com/example/alexsteen/hackgt16/WelcomeScreen.java
@@ -1,8 +1,12 @@
 package com.example.alexsteen.hackgt16;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class WelcomeScreen extends AppCompatActivity {
@@ -13,6 +17,10 @@ public class WelcomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //Button b = (Button) findViewById(R.id.next);
     }
+
+
+
 }
 
