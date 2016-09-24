e7498352af974bdacda46ec8db7ebe92a9695366
ericachia <ercachia@gmail.com>
2016-09-24 18:16:13 -0400

    starting fitness home page

---
+++ app/src/main/java/com/example/alexsteen/hackgt16/FitnessHomepage.java
@@ -0,0 +1,22 @@
+package com.example.alexsteen.hackgt16;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FitnessHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness_homepage);
+    }
+
+    public void goToChecklist(View view) {
+        Intent intent = new Intent(this, Checklist.class);
+        startActivity(intent);
+    }
+
+    
+}
