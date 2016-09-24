cd1c50f929d1c40872a1e774d9208988bf977501
jialee <alicia15kc@gmail.com>
2016-09-24 15:27:35 -0400

    Added welcome screen activity.

--- app/src/main/java/com/example/alexsteen/hackgt16/MainActivity.java
+++ app/src/main/java/com/example/alexsteen/hackgt16/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.alexsteen.hackgt16;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.graphics.Typeface;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 //what dis
@@ -12,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button)findViewById(R.id.button2);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/Cabin-Regular.ttf");
+        login.setTypeface(custom_font);
+
+        Button register = (Button)findViewById(R.id.button);
+        Typeface custom_font2 = Typeface.createFromAsset(getAssets(), "fonts/Cabin-Regular.ttf");
+        register.setTypeface(custom_font2);
+
+
     }
+
+    public void goToLoginScreen(View view) {
+        Intent i = new Intent(this, LoginScreen.class);
+        startActivity(i);
+    }
+
 }
