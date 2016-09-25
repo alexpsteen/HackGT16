188351800abb3d57626f695b3b0f1b5d8f37dada
ericachia <ercachia@gmail.com>
2016-09-24 20:26:43 -0400

    starting fitness home page

---
+++ app/src/main/java/com/example/alexsteen/hackgt16/Timer.java
@@ -0,0 +1,17 @@
+package com.example.alexsteen.hackgt16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+
+
+
+
+}
