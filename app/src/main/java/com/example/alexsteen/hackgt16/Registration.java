889cbdbdef0ea05a08efbbb947363c18d652905a
jialee <alicia15kc@gmail.com>
2016-09-24 22:27:46 -0400

    Completed UI buttons for login and registration, and are linked to welcome page.

--- app/src/main/java/com/example/alexsteen/hackgt16/Registration.java
+++ app/src/main/java/com/example/alexsteen/hackgt16/Registration.java
@@ -2,12 +2,34 @@ package com.example.alexsteen.hackgt16;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.view.View;
+import android.content.Intent;
 
 public class Registration extends AppCompatActivity {
+
+    boolean consultant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        consultant = false;
     }
+
+    public boolean consCheck() {
+        CheckBox cb = (CheckBox) findViewById(R.id.consultant);
+        if (cb.isChecked()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void goToWelcomeScreen(View view) {
+        Intent i = new Intent(this, WelcomeScreen.class);
+        startActivity(i);
+    }
+
 }
