34143bc1793dc07baee0400296931988f24a2642
jialee <alicia15kc@gmail.com>
2016-09-24 23:20:34 -0400

    Updating consultant ID, and starting to connect DB to front end.

--- app/src/main/java/com/example/alexsteen/hackgt16/Registration.java
+++ app/src/main/java/com/example/alexsteen/hackgt16/Registration.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.widget.CheckBox;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 public class Registration extends AppCompatActivity {
 
     boolean consultant;
+    private userDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         consultant = false;
+        db = new userDB(this);
     }
 
     public boolean consCheck() {
@@ -31,5 +34,28 @@ public class Registration extends AppCompatActivity {
         Intent i = new Intent(this, WelcomeScreen.class);
         startActivity(i);
     }
+
+    public void createUser() {
+        EditText aName = (EditText)findViewById(R.id.fullnameblank);
+        EditText aUserName = (EditText)findViewById(R.id.usernameblank2);
+        EditText aPassWord = (EditText)findViewById(R.id.passwordblank2);
+        EditText aAge = (EditText)findViewById(R.id.ageblank);
+        EditText aFeet = (EditText)findViewById(R.id.feetblank);
+        EditText aInches = (EditText)findViewById(R.id.inchesblank);
+        EditText aWeight = (EditText)findViewById(R.id.weightblank);
+
+        String n = aName.getText().toString();
+        String un = aUserName.getText().toString();
+        String pw = aPassWord.getText().toString();
+        String a = aAge.getText().toString();
+        String ft = aFeet.getText().toString();
+        String in = aInches.getText().toString();
+        String wt = aWeight.getText().toString();
+        boolean aConsultant = consCheck();
+
+
+    }
+
+
 
 }
